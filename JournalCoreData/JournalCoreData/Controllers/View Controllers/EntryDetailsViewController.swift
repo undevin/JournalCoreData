075c448dc02d2674e2825f69576e179d47804f98
Devin Flora <devinlflora@gmail.com>
2021-01-18 18:14:42 -0600

    Tweaked storyboard and code cleanup

--- JournalCoreData/JournalCoreData/Controllers/View Controllers/EntryDetailsViewController.swift
+++ JournalCoreData/JournalCoreData/Controllers/View Controllers/EntryDetailsViewController.swift
@@ -17,6 +17,7 @@ class EntryDetailsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         updateViews()
+        bodyTextField.layer.borderWidth = 1
     }
     
     // MARK: - Properties
